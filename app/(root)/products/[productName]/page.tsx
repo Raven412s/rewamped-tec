381e6b7a5c3b661b841a9d64089185edb24b2cd5
Ashutosh Sharan <being.ashutosh16.20@gmail.com>
2025-02-22 03:16:41 +0530

    SEO STAGE 2 check

--- app/(root)/products/[productName]/page.tsx
+++ app/(root)/products/[productName]/page.tsx
@@ -41,7 +41,7 @@ export async function generateMetadata({ params }: ProductPageProps): Promise<Me
       openGraph: {
         type: "website",
         url: `${siteDomain}/products/${params.productName}`,
-        title: `${product.productTitle} - The Elevator Company`,
+        title: `${product.productTitle} - Product - The Elevator Company`,
         description: product.productDescription,
         images: [
           {
